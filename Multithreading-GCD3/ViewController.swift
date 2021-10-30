edc4692a23709407f5b04313ed2a8c271fa465b8
Rus <de.mr44@gmail.com>
2021-10-30 20:32:15 +0500

    Added an inactive queue

--- Multithreading-GCD3/ViewController.swift
+++ Multithreading-GCD3/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+        
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    private let inactiveQueue = DispatchQueue(label: "inactive", attributes: [.concurrent, .initiallyInactive])
+    private var number = 0 {
+        didSet {
+            DispatchQueue.main.sync {
+                label.text = number.description
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        button.layer.cornerRadius = 15
     }
-
-
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        
+    }
 }
 
